ff9e519ab469ff036d99c860f8ec0389da037059
Brim-Hub <diwakarsurajb@gmail.com>
2025-01-18 17:04:24 +0530

    component 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Component1 from './components/Component1';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h1>hi Suraj</h1>
+      <Component1/>
       </header>
+
     </div>
   );
 }
